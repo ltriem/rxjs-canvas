3a886e57bf59e87e5c98cab0c4595a0f51b919df
Anastasia Sadovaya <stase4ka@MacBook-Air-Anastasia.local>
2020-03-31 15:01:15 +0300

    add mousemove & down

---
+++ src/index.js
@@ -0,0 +1,38 @@
+import {fromEvent} from 'rxjs';
+import {map, pairwise, switchMap} from 'rxjs/operators'
+
+
+const canvas = document.querySelector('canvas');
+
+const ctx = canvas.getContext('2d');
+const rect = canvas.getBoundingClientRect();
+const scale = window.devicePixelRatio;
+
+canvas.width = rect.width * scale;
+canvas.height = rect.height * scale;
+
+ctx.scale(scale, scale);
+
+const mouseMove$ = fromEvent(canvas, 'mousemove');
+const mouseDown$ = fromEvent(canvas, 'mousedown');
+
+const stream$ = mouseDown$
+    .pipe(
+        switchMap(() => {
+            return mouseMove$
+            .pipe(
+                map(e => ({
+                    x: e.offsetX,
+                    y: e.offsetY,
+                })),
+                pairwise()
+            )
+        })
+    )
+
+stream$.subscribe(([from, to]) => {
+    ctx.beginPath()
+    ctx.moveTo(from.x, from.y)
+    ctx.lineTo(to.x, to.y)
+    ctx.stroke()
+})
